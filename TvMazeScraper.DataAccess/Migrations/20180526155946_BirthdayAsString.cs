2f10d99e908df0c01d9e521a2045ea26e166098c
Viktar Tserashchuk <Viktar_Tserashchuk@epam.com>
2018-05-26 21:14:54 +0300

    Add migration to store the birthday as a string

---
+++ TvMazeScraper.DataAccess/Migrations/20180526155946_BirthdayAsString.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TvMazeScraper.DataAccess.Migrations
+{
+    public partial class BirthdayAsString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Birthday",
+                table: "Actors",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Actors",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
